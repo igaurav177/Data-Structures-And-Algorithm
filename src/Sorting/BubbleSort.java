5be84acfdcb662171774a4be9708135dec91af53
igaurav177 <gauravkumarsharma2001@gmail.com>
2023-05-23 14:04:29 +0530

    modification in package files

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[]={2,6,4,8,3,1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubbleSort(int[] arr){
+        boolean swapped=false;
+        for(int i=0;i<arr.length;i++){
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+}
